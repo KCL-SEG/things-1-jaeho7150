a7f321a6758d1aa91df3c4e6c791315176383288
Jaeho Lee <jaeho.lee@kcl.ac.uk>
2022-10-31 13:43:10 +0000

    impot Models class from models module into models.py

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 # Create your models here.
 
-class Thing(models.Models):
+class Thing(models.Model):
     name = models.CharField()
     description = models.CharField()
     quantity = models.IntegerField()
